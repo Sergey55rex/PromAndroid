f84cb0434da6efed6606620eb3295f2a5e60b5f7
Sergey Bobkov <rex55@mail.ru>
2021-05-08 19:52:20 +0300

    v 7.1

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -17,7 +17,7 @@ class PostRepositoryImpl: PostRepository {
     private val gson = Gson()
     private val typeToken = object : TypeToken<List<Post>>() {}
 
-
+//jhjhjhjhjhjhjhjhjhj
     companion object {
         internal const val BASE_URL = "http://10.0.2.2:9999"
         private val jsonType = "application/json".toMediaType()
